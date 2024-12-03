cb8de7509a246e801bfafc77b25352547c0ab4f1
Axel Várai <axelvgames@gmail.com>
2024-12-03 22:15:47 +0300

    Deploy

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,16 +1,17 @@
 // @ts-check
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://github.com/Astrohelo",
   integrations: [
     icon({
       include: {
-        mdi: ['*'] // This includes all MDI icons
-      }
-    })
-  ]
+        mdi: ["*"], // This includes all MDI icons
+      },
+    }),
+  ],
 });
