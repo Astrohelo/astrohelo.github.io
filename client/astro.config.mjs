f65ab60e4f45091fb4e33e0717d35c4c660998c6
Axel Várai <axelvgames@gmail.com>
2024-12-03 21:54:37 +0300

    Update

---
+++ client/astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    icon({
+      include: {
+        mdi: ['*'] // This includes all MDI icons
+      }
+    })
+  ]
+});
